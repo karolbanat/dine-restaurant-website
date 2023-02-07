0a22e3cae2682452900f2b9ceed570be9c1c7c89
Karol Banat <karol.banat@gmail.com>
2023-02-07 17:58:00 +0100

    Custom select

--- src/ts/reservation-form.ts
+++ src/ts/reservation-form.ts
@@ -2,15 +2,41 @@ const customSelect: HTMLElement = document.querySelector('.custom-select');
 const originalSelect: HTMLSelectElement = customSelect.querySelector('select');
 const originalSelectOptions: Array<HTMLOptionElement> = Array.from(originalSelect.querySelectorAll('option'));
 
+const customSelectFocus: HTMLElement = customSelect.querySelector('.custom-select__focus');
+const customSelectFocusText: HTMLElement = customSelectFocus.querySelector('span');
 const customSelectOptionsContainer: HTMLElement = customSelect.querySelector('.custom-select__options');
+let customSelectOptions: Array<HTMLElement> = [];
 
 const CHECK_ICON_URL: string = './dist/assets/images/icons/icon-check.svg';
 
 createCustomSelectOptions();
 
+/* event listeners */
+originalSelect.addEventListener('keyup', (e: KeyboardEvent) => {
+	switch (e.key) {
+		case 'ArrowUp':
+		case 'ArrowDown':
+			handleSelection();
+			break;
+		case 'Enter':
+			handleSelection();
+			closeCustomSelect();
+			break;
+	}
+});
+
+customSelectFocus.addEventListener('click', (e: Event) => {
+	originalSelect.focus();
+});
+
+originalSelect.addEventListener('focusin', openCustomSelect);
+
+/* helper functions */
 function createCustomSelectOptions(): void {
 	originalSelectOptions.forEach(option => {
-		customSelectOptionsContainer.appendChild(createCustomOption(option));
+		const customOption: HTMLElement = createCustomOption(option);
+		customSelectOptions.push(customOption);
+		customSelectOptionsContainer.appendChild(customOption);
 	});
 }
 
@@ -19,6 +45,7 @@ function createCustomOption(option: HTMLOptionElement): HTMLElement {
 	optionWrapper.classList.add('custom-select__option');
 	optionWrapper.setAttribute('data-selected', option.selected.toString());
 	optionWrapper.setAttribute('data-value', option.value);
+	optionWrapper.addEventListener('click', handleCustomOption);
 
 	const checkImage = document.createElement('img');
 	checkImage.setAttribute('src', CHECK_ICON_URL);
@@ -31,4 +58,27 @@ function createCustomOption(option: HTMLOptionElement): HTMLElement {
 	optionWrapper.appendChild(optionText);
 
 	return optionWrapper;
+}
+
+function handleCustomOption(e: Event): void {
+	const option: HTMLElement = e.target as HTMLElement;
+	originalSelectOptions.forEach(opt => (opt.selected = opt.value === option.dataset.value));
+	handleSelection();
+	closeCustomSelect();
+}
+
+function handleSelection(): void {
+	const selectedValue: string = originalSelectOptions.filter(opt => opt.selected)[0].value;
+	customSelectFocusText.innerText = selectedValue.toUpperCase();
+	customSelectOptions.forEach(option => {
+		option.dataset.selected = (option.dataset.value === selectedValue).toString();
+	});
+}
+
+function openCustomSelect(): void {
+	customSelect.dataset.open = 'true';
+}
+
+function closeCustomSelect(): void {
+	customSelect.dataset.open = 'false';
 }
